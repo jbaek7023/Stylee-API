50872240ab47f74bde52fc7a9b456d1577b77389
John Baek <jbaek7023@gmail.com>
2017-08-09 15:04:48 +0900

    jwt works with python3 + JWT testing doc updated

--- stylee/stylee/urls.py
+++ stylee/stylee/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-# import JWT 
+# import JWT
 from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
